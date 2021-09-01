4fe8ee70a945f1c51d5b357b946a381fc38e31ff
ArthurMN <56900887+ArthurMN@users.noreply.github.com>
2021-09-01 17:49:32 -0300

    Update models.py

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import sqlite3
 import os
 from sys import path
 
+
 ROOT = os.path.dirname(os.path.realpath(__file__))
 
 def create_course(nome_curso, nome_facilitador, preco, data, link, validacao):
@@ -13,7 +14,3 @@ def create_course(nome_curso, nome_facilitador, preco, data, link, validacao):
     con.commit()
     con.close()
     
-def get_course():
-    con = sqlite3.connect(os.path.join(ROOT, 'database.db'))
-    cur = con.cursor()
-    cur.execute("SELECT * FROM cursos")
