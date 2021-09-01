2d6b5f91b0cf68fcbe97c895dd2687e78925ee99
ArthurMN <56900887+ArthurMN@users.noreply.github.com>
2021-08-31 23:35:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 
-from models import create_course
+import sqlite3
+
+from werkzeug.utils import redirect
+from models import create_course, get_course
 from flask import Flask, render_template, url_for, request
 
 
@@ -11,27 +14,43 @@ app = Flask(__name__)
 def homepage():
     return render_template("tela1.html")
 
-@app.route('/tela2' , methods = ['GET', 'POST'])
+@app.route('/tela2')
 def register_page():
+    return render_template("tela2.html")
+
+@app.route('/cadastro', methods = ['GET', 'POST'])
+def register():
     if request.method == 'POST':
         nome_curso = request.form.get('nome_curso')
         nome_facilitador = request.form.get('nome_facilitador')
         link = request.form.get('link')
-        data = request.form.get('data')
+        data =  request.form.get('data')
         preco = request.form.get('preco')
         validacao = request.form.get('validacao')
-        create_course(nome_curso, nome_facilitador, preco, data, link, validacao)
-        
-    return render_template("tela2.html")
+        create_course(nome_curso, nome_facilitador, preco, data, link, validacao) 
+    return redirect(url_for("publication_effected"))
+
 
 @app.route('/tela3')
-def feed():
-    return render_template("tela3.html")
+def list():
+    con = sqlite3.connect("database.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("SELECT * FROM cursos")
+    rows = cur.fetchall()
+    return render_template("tela3.html", rows = rows)
 
-@app.route('/tela4')
+
+@app.route('/tela4', methods= ['POST','GET'])
 def publication_effected():
+    if request.method == 'POST':
+        if request.form.get('acao1'):
+           return redirect(url_for("homepage"))
+
+        if request.form.get('acao2'):
+            return redirect(url_for('register_page'))
+
     return render_template("tela4.html")
     
-
 if __name__ == "__main__":
     app.run(debug=True)
